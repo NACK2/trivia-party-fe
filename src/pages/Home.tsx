a297a6ee7a47e70b7b89bc3f2ea6dafea3d5fce2
Nicholas Kang <nicholaskang5@gmail.com>
2025-05-09 23:29:13 -0700

    revamped home page using mui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,63 +3,67 @@ import Typography from "@mui/material/Typography";
 
 function Home() {
   return (
-    <>
-      <div className="flex flex-col h-screen">
-        <div className="flex">
-          <Typography
-            variant="h1"
-            sx={{
-              ...headerStyles,
-              bgcolor: "primary.main",
-              justifyContent: "end",
-              pr: 4,
-            }}
-          >
-            TRIVIA
-          </Typography>
-          <Typography
-            variant="h1"
-            sx={{
-              ...headerStyles,
-              bgcolor: "primary.dark",
-              justifyContent: "start",
-              pl: 4,
-            }}
-          >
-            PARTY
-          </Typography>
-        </div>
-        <div className="flex h-full">
-          <Box
-            sx={{
-              ...cardStyles,
-              bgcolor: "primary.main",
-            }}
-            onClick={() => {console.log('lobby')}}
-          >
-            LOBBY
-          </Box>
-          <Box
-            sx={{
-              ...cardStyles,
-              bgcolor: "primary.dark",
-            }}
-            onClick={() => {console.log('online')}}
-          >
-            ONLINE
-          </Box>
-        </div>
-      </div>
-    </>
+    <Box sx={{ bgcolor: "primary.main", height: "100vh" }}>
+      <Box
+        sx={{
+          display: "flex",
+          fontFamily: "Bungee, sans-serif",
+        }}
+      >
+        <Typography
+          variant="h1"
+          sx={{
+            display: "flex",
+            justifyContent: "end",
+            fontFamily: "inherit",
+            width: "50%",
+            color: "secondary.main",
+            pr: 4,
+          }}
+        >
+          TRIVIA
+        </Typography>
+        <Typography
+          variant="h1"
+          sx={{
+            display: "flex",
+            bgcolor: "primary.dark",
+            justifyContent: "start",
+            fontFamily: "inherit",
+            width: "50%",
+            color: "secondary.main",
+            pl: 4,
+          }}
+        >
+          PARTY
+        </Typography>
+      </Box>
+      <Box sx={{ display: "flex", height: "100%" }}>
+        <Box
+          sx={{
+            ...cardStyles,
+          }}
+          onClick={() => {
+            console.log("lobby");
+          }}
+        >
+          SINGLE PLAYER
+        </Box>
+        <Box
+          sx={{
+            ...cardStyles,
+            bgcolor: "primary.dark",
+          }}
+          onClick={() => {
+            console.log("online");
+          }}
+        >
+          ONLINE
+        </Box>
+      </Box>
+    </Box>
   );
 }
-
-const headerStyles = {
-  fontFamily: "Bungee, sans-serif",
-  color: "secondary.main",
-  width: "50%",
-  display: "flex",
-};
 
 const cardStyles = {
   fontFamily: "Bungee, sans-serif",
---
+++ src/pages/Singleplayer.tsx
@@ -0,0 +1,9 @@
+import Typography from '@mui/material/Typography';
+
+function Singleplayer() {
+    return (
+        <Typography color='primary.main'>SINGLE PLAYER</Typography>
+    )
+}
+
+export default Singleplayer

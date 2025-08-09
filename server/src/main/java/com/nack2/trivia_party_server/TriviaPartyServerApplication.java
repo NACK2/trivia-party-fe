b5d9eab06770d1137a203158c7b153f6627e16a2
Nicholas Kang <nicholaskang5@gmail.com>
2025-08-09 12:28:41 -0700

    created test hello world endpoint to test

--- server/src/main/java/com/nack2/trivia_party_server/TriviaPartyServerApplication.java
+++ server/src/main/java/com/nack2/trivia_party_server/TriviaPartyServerApplication.java
@@ -2,7 +2,12 @@ package com.nack2.trivia_party_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api/v1")
 @SpringBootApplication
 public class TriviaPartyServerApplication {
 
@@ -10,4 +15,8 @@ public class TriviaPartyServerApplication {
 		SpringApplication.run(TriviaPartyServerApplication.class, args);
 	}
 
+	@GetMapping()
+	public String getHelloWorld() {
+		return "Hello World";
+	}
 }

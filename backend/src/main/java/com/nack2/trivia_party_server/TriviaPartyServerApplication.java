7b852b12742583878280792099a44c03db8539f3
Nicholas Kang <nicholaskang5@gmail.com>
2025-08-08 18:06:51 -0700

    added springboot backend + created root gitignore file

---
+++ backend/src/main/java/com/nack2/trivia_party_server/TriviaPartyServerApplication.java
@@ -0,0 +1,13 @@
+package com.nack2.trivia_party_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaPartyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaPartyServerApplication.class, args);
+	}
+
+}

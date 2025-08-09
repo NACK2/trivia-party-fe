5457aa19cad82f16b3448cc9afa5f998ed70bc66
Nick Kang <56712228+NACK2@users.noreply.github.com>
2025-08-08 20:41:59 -0700

    Initialized Spring Boot server side files + Containerized Postgres DB (#16)
    * added springboot backend + created root gitignore file
    
    * updated folder structure to client and server
    
    * update docker files to use "client"
    
    * setup docker for postgres server
    
    * edited the readme to reflect new docker service changes

---
+++ server/src/test/java/com/nack2/trivia_party_server/TriviaPartyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nack2.trivia_party_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaPartyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
